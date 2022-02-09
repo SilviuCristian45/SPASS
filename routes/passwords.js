d00ac8423750425228a69f9bec36fb2dbeca9352
DESKTOP-LDQLJQC\Silviu <silviudinca412@gmail.com>
2022-02-09 14:08:42 +0200

    Db logic functions moved in password controller
    The CRUD functionality will stay into the ppasswordController.js file

--- routes/passwords.js
+++ routes/passwords.js
@@ -5,34 +5,10 @@ const router = express.Router()
 let dbObj;
 require('../database').then( db => dbObj = db);
 
-// input : db - database object
-// output : a promise containing all the passwords from database 
-async function getPasswords(db){
-    const cursor = db.collection("passwords").find({});
-    const result = await cursor.toArray()
-    return result
-}
+const passwordController = require('../controllers/passwordController');
 
-// input : db - database object
-//         serviceName - google/facebook/instagram etc
-//         username/email - based on the user account on that service
-//         password - based on the user account 
-//         userid - the user which created this account 
-// output : a promise containing the created password
-function addPassword(db, serviceName, username, password, userid){
-    newAccount = {
-        service:serviceName,
-        username:username,
-        password:password,
-        userid:userid
-    }
-    return new Promise( (resolve, reject) => { 
-        db.collection("passwords").insertOne(newAccount, (err, res) => {
-        if(err) reject(err);
-        return resolve('succes')
-        });
-    });
-}
+const getPasswords = passwordController.getPasswords;
+const addPassword =  passwordController.addPassword;
 
 router.get('/', async (req, res) => {
     getPasswords(dbObj).then( (result) => {
