8300179849dddc2367204bfe784e01b373130783
Silviu <silviudinca412@gmail.com>
2022-08-29 20:58:35 +0300

    removed dist because it's unnecesary

--- dist/controllers/utilController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateAccount = void 0;
-function validateAccount(account) {
-    const { service, username, password, userid } = account;
-    //console.log(typeof(service.length));
-    return !(service.length == 0 || username.length == 0 || password.length == 0);
-}
-exports.validateAccount = validateAccount;
